2ff6ced8b0b6cb3f3fc02eb6b64aeb829416c788
Gustavo Mansur de Oliveira <78883690+GMansur7@users.noreply.github.com>
2021-11-21 12:29:24 -0300

    Add files via upload

--- ESP32-SIM800L-Firebase-sender/ESP32-SIM800L-Firebase-sender.ino
+++ ESP32-SIM800L-Firebase-sender/ESP32-SIM800L-Firebase-sender.ino
@@ -1,74 +1,68 @@
+
+/* Programa de envio de dados ao Firebase via HTTP com rede GPRS (Telefonia móvel)
+ * 
+ * link do tutorial: https://github.com/vshymanskyy/TinyGSM
+ * 
+ * Autor: Gustavo Mansur de Oliveira ( E-mail: gustavo.mansuroliveira@gmail.com)
+ * 
+ */
+
 //Select your modem
 //SSL/TLS is currently supported only with SIM8xx series
 #define TINY_GSM_MODEM_SIM800
 
 //Increase RX buffer
-//#define TINY_GSM_RX_BUFFER 256 
+//#define TINY_GSM_RX_BUFFER 256
 
-//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//***********************************************************************
 //#include <TinyGPS++.h> //https://github.com/mikalhart/TinyGPSPlus
 #include <TinyGsmClient.h> //https://github.com/vshymanskyy/TinyGSM
 #include <ArduinoHttpClient.h> //https://github.com/arduino-libraries/ArduinoHttpClient
-//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//***********************************************************************
 
-//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-const char FIREBASE_HOST[]  = "xxxxxxxxxxxxxxxxxx.firebaseio.com";
-const String FIREBASE_AUTH  = "xxxxxxxxxxxxxxxxxxxxxxxxx";
+//***********************************************************************
+const char FIREBASE_HOST[]  = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx.firebaseio.com";
+const String FIREBASE_AUTH  = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 //const String FIREBASE_PATH  = "dados/coordenadas";
 const String FIREBASE_PATH  = "/";
 const int SSL_PORT          = 443;
-//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//***********************************************************************
 
-//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//***********************************************************************
 // Your GPRS credentials
 // Leave empty, if missing user or pass
 char apn[]  = "GPRS internet";
 char user[] = "";
 char pass[] = "";
-//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//***********************************************************************
 
+//***********************************************************************
 
-//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-//GSM Module RX pin to ESP32 2
-//GSM Module TX pin to ESP32 4
-//#define rxPin 26
-//#define txPin 27
-
-//pino LED embarcado no pino 13
+//Pino LED embarcado no pino 13
 #define LED 13
 
+//Para modulos SIM800L não integrados definir os pinos de RX e TX
+//GSM Module RX pin to ESP32 2
+//GSM Module TX pin to ESP32 4
 
-//Pinos SIM800L
-#define MODEM_RST 5
-#define MODEM_PWRKEY 4
+//Pinos SIM800L modulo integrado
 #define MODEM_POWER_ON 23
+#define MODEM_PWRKEY 4
+#define MODEM_RST 5
 #define MODEM_TX 27
 #define MODEM_RX 26
 
 HardwareSerial sim800(1);
 TinyGsm modem(sim800);
-//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-/*
-//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-//GPS Module RX pin to ESP32 17
-//GPS Module TX pin to ESP32 16
-#define RXD2 16
-#define TXD2 17
-HardwareSerial neogps(2);
-TinyGPSPlus gps;
-//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-*/
-//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//***********************************************************************
 TinyGsmClientSecure gsm_client_secure_modem(modem, 0);
 HttpClient http_client = HttpClient(gsm_client_secure_modem, FIREBASE_HOST, SSL_PORT);
-//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//***********************************************************************
 
-//unsigned long previousMillis = 0;
-//long interval = 10000;
-
-//**************************************************************************************************
+//***********************************************************************
 void setup() {
 
+  //set do LED do pino 13
   pinMode(LED, OUTPUT);
   
   //Reseta o SIM800L
@@ -83,18 +77,13 @@ void setup() {
   delay(1000);
   digitalWrite(MODEM_PWRKEY, HIGH);
 
-
   Serial.begin(115200);
   Serial.println("esp32 serial initialize");
   
   sim800.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
   Serial.println("SIM800L serial initialize");
-/*
-  neogps.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  Serial.println("neogps serial initialize");
-  delay(3000);
-*/  
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  
+  //***********************************************************************
   //Restart takes quite some time
   //To skip it, call init() instead of restart()
   Serial.println("Initializing modem...");
@@ -102,19 +91,18 @@ void setup() {
   String modemInfo = modem.getModemInfo();
   Serial.print("Modem: ");
   Serial.println(modemInfo);
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  //***********************************************************************
   
   // Unlock your SIM card with a PIN
   //modem.simUnlock("1234");
   
   http_client.setHttpResponseTimeout(90 * 1000); //^0 secs timeout
 }
-//**************************************************************************************************
 
-
-//**************************************************************************************************
+//*************************************************************************
 void loop() {
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  
+  //***********************************************************************
   //Restart takes quite some time
   //To skip it, call init() instead of restart()
   //Serial.println("Initializing modem...");
@@ -122,9 +110,7 @@ void loop() {
   //String modemInfo = modem.getModemInfo();
   //Serial.print("Modem: ");
   //Serial.println(modemInfo);
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
   
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
   //Serial.print(F("Waiting for network..."));
   //if (!modem.waitForNetwork()) {
     //Serial.println(" fail");
@@ -132,9 +118,8 @@ void loop() {
     //return;
   //}
   //Serial.println(" OK");
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
     
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  //***********************************************************************
   Serial.print(F("Connecting to "));
   Serial.print(apn);
   if (!modem.gprsConnect(apn, user, pass)) {
@@ -143,11 +128,11 @@ void loop() {
     return;
   }
   Serial.println(" OK");
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  //***********************************************************************
   
   http_client.connect(FIREBASE_HOST, SSL_PORT);
   
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+  //***********************************************************************
   while (true) {
     if (!http_client.connected()) {
       Serial.println();
@@ -156,20 +141,20 @@ void loop() {
       break;
     }
     else{
-      gps_loop();
+      acquisition_loop();
     }
   }
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+  //***********************************************************************
 }
-//**************************************************************************************************
+  //***********************************************************************
 
-//**************************************************************************************************
+//***********************************************************************
 void PostToFirebase(const char* method, const String & path , const String & data, HttpClient* http) {
   String response;
   int statusCode = 0;
   http->connectionKeepAlive(); // Currently, this is needed for HTTPS
   
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  //***********************************************************************
   String url;
   if (path[0] != '/') {
     url = "/";
@@ -180,19 +165,21 @@ void PostToFirebase(const char* method, const String & path , const String & dat
   Serial.println(url);
   Serial.print("Data:");
   Serial.println(data);
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  //***********************************************************************
   
   String contentType = "application/json";
   http->put(url, contentType, data);
-//  http->put("/.json?auth=Legd1ze7Y5xDqgZQxdN23zWPvv791FZe0MSuNv26", "application/json", "{\"lat\":41.2,\"lng\":52.1}"); // exemplo de preenchimento
+  
+//http->put("/.json?auth=Legd1ze7Y5xDqgZQxdN23zWPvv791FZe0MSuNv26", "application/json", "{\"lat\":41.2,\"lng\":52.1}"); // exemplo de preenchimento
 //lilygo-esp32-sim800l-default-rtdb.firebaseio.com/.json?auth=Legd1ze7Y5xDqgZQxdN23zWPvv791FZe0MSuNv26/application/json/{\"lat\":41.2,\"lng\":52.1}
 //link para navegador:lilygo-esp32-sim800l-default-rtdb.firebaseio.com/.json?=Legd1ze7Y5xDqgZQxdN23zWPvv791FZe0MSuNv26/application/json/{\"lat\":80.2,\"lng\":52.1}
+
+//Pisca LED pino 13 quando envia os dados
   digitalWrite(LED, HIGH);
   delay(100);
   digitalWrite(LED, LOW);
-
   
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  //***********************************************************************
   // read the status code and body of the response
   //statusCode-200 (OK) | statusCode -3 (TimeOut)
   statusCode = http->responseStatusCode();
@@ -201,80 +188,55 @@ void PostToFirebase(const char* method, const String & path , const String & dat
   response = http->responseBody();
   Serial.print("Response: ");
   Serial.println(response);
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  //***********************************************************************
 
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  //***********************************************************************
   if (!http->connected()) {
     Serial.println();
     http->stop();// Shutdown
     Serial.println("HTTP POST disconnected");
   }
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  //***********************************************************************
 }
-//**************************************************************************************************
 
+//*************************************************************************
 
-//**************************************************************************************************
-
-void gps_loop()
+void acquisition_loop()
 {
-  /*
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-  //Can take up to 60 seconds
-  boolean newData = false;
-  for (unsigned long start = millis(); millis() - start < 2000;){
-    while (neogps.available()){
-      if (gps.encode(neogps.read())){
-        newData = true;
-        break;
-      }
-    }
-  }
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
 
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-  //If newData is true
-  if(true){
-  newData = false;
-  */
   String latitude = "47.2";
   String longitude = "46.4";
+  
+  //Exemplos de formatação de dados
   //float altitude;
   //unsigned long date, time, speed, satellites;
-  
+
+  //Exemplos de formatação de dados
   //latitude = String(gps.location.lat(), 6); // Latitude in degrees (double)
   //longitude = String(gps.location.lng(), 6); // Longitude in degrees (double)
 
   //latitude = String(gps.location.lat(), 6); // Latitude in degrees (double)
   //longitude = String(gps.location.lng(), 6); // Longitude in degrees (double)
 
-  
-  //altitude = gps.altitude.meters(); // Altitude in meters (double)
-  //date = gps.date.value(); // Raw date in DDMMYY format (u32)
-  //time = gps.time.value(); // Raw time in HHMMSSCC format (u32)
-  //speed = gps.speed.kmph();
   
   Serial.print("Latitude= "); 
   Serial.print(latitude);
   Serial.print(" Longitude= "); 
   Serial.println(longitude);
       
-  String gpsData = "{";
-  gpsData += "\"lat\":" + latitude + ",";
-  gpsData += "\"lng\":" + longitude + "";
-  gpsData += "}";
+  String Data = "{";
+  Data += "\"lat\":" + latitude + ",";
+  Data += "\"lng\":" + longitude + "";
+  Data += "}";
 
-  //PUT   Write or replace data to a defined path, like messages/users/user1/<data>
+  //PUT     Write or replace data to a defined path, like messages/users/user1/<data>
   //PATCH   Update some of the keys for a defined path without replacing all of the data.
-  //POST  Add to a list of data in our Firebase database. Every time we send a POST request, the Firebase client generates a unique key, like messages/users/<unique-id>/<data>
+  //POST    Add to a list of data in our Firebase database. Every time we send a POST request, the Firebase client generates a unique key, like messages/users/<unique-id>/<data>
   //DELETE  Remove data from the Firebase database reference reference.
   //https://firebase.google.com/docs/database/rest/save-data
   
-  PostToFirebase("PATCH", FIREBASE_PATH, gpsData, &http_client);
+  PostToFirebase("PATCH", FIREBASE_PATH, Data, &http_client);
   
-
- // }
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
 }
-
-//**************************************************************************************************
+//Fim
+//******************************************************************************
